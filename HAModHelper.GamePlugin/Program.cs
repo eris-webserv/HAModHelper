b937b502447ad91732f55c6d5778710ec606685d
Eris <eris@erisws.com>
2026-02-27 17:00:49 -0500

    Whoops.

--- HAModHelper.GamePlugin/Program.cs
+++ HAModHelper.GamePlugin/Program.cs
@@ -2,4 +2,4 @@ using MelonLoader;
 using HAModHelper.GamePlugin.Core; // The namespace of your mod class
 
 [assembly: MelonInfo(typeof(HAMHMod), "HAModHelper", "v0.0.1", "Eris (erisws)")]
-[assembly: MelonGame("Abstract Softwares", "Hybrid Animals")]
+[assembly: MelonGame("Abstract Softwares", "HAModLoader")]
