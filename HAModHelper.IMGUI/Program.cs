7e3e9f85092c9c63fd11e25a1b93295aee36d30b
Eris <eris@erisws.com>
2026-03-11 01:58:39 -0400

    he realized he not built for this

--- HAModHelper.IMGUI/Program.cs
+++
@@ -1,7 +0,0 @@
-
-using MelonLoader;
-using HAModHelper.GamePlugin.Core; // The namespace of your mod class
-
-[assembly: HarmonyDontPatchAll]  // manual patching handled during init
-[assembly: MelonInfo(typeof(HAMHMod), "HAModHelper.IMGUI", "0.0.1", "Eris (erisws)")]
-[assembly: MelonGame("Abstract Software Inc", "Hybrid Animals")]
